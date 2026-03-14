9b21d99ac769a731093e50e9783ecc3ba7c93040
SagiBeno <benosagi321@gmail.com>
2026-03-14 11:35:40 +0100

    Add categories endpoint test

---
+++ tests/categories.test.js
@@ -0,0 +1,17 @@
+import request from 'supertest';
+import { describe, it, expect } from 'vitest';
+import { app } from '../index.cjs';
+
+describe('Categories endpoint', () => {
+
+    it('GET /categories should returncategory list', async () => {
+        const response = await request(app).get('/categories');
+
+        expect(response.status).toBe(200);
+        expect(Array.isArray(response.body)).toBe(true);
+        expect(response.body.length).toBeGreaterThan(0);
+
+        expect(response.body[0]).toHaveProperty('id');
+        expect(response.body[0]).toHaveProperty('name');
+    });
+});
